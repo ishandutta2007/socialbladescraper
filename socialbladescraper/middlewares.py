2fb73ba28c5fd8e15c125e835626b6a18d69adcb
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-10-27 09:48:59 +0530

    all social

--- socialbladescraper/middlewares.py
+++ socialbladescraper/middlewares.py
@@ -53,7 +53,7 @@ class SocialbladescraperSpiderMiddleware(object):
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class SocialbladescraperDownloaderMiddleware(object):
@@ -100,4 +100,4 @@ class SocialbladescraperDownloaderMiddleware(object):
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
--- socialbladescraper/spiders/top5000_spider.py
+++ socialbladescraper/spiders/top5000_spider.py
@@ -7,19 +7,42 @@ class Top5000Spider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://socialblade.com/youtube/top/5000/',
-            'https://socialblade.com/youtube/top/5000/mostviewed/',
-            'https://socialblade.com/youtube/top/5000/mostsubscribed/',
+            "https://socialblade.com/youtube/top/5000",
+            "https://socialblade.com/youtube/top/5000/mostviewed",
+            "https://socialblade.com/youtube/top/5000/mostsubscribed",
+            "https://socialblade.com/instagram/top/500",
+            "https://socialblade.com/instagram/top/500/followers",
+            "https://socialblade.com/instagram/top/500/following",
+            "https://socialblade.com/instagram/top/500/media",
+            "https://socialblade.com/instagram/top/500/engagement-rate",
+            "https://socialblade.com/twitter/top/100/tweets",
+            "https://socialblade.com/twitter/top/100/engagements",
+            "https://socialblade.com/twitter/top/100/following",
+            "https://socialblade.com/twitter/top/100/followers",
+            "https://socialblade.com/facebook/top/500",
+            "https://socialblade.com/facebook/top/500/likes",
+            "https://socialblade.com/dailymotion",
+            "https://socialblade.com/mixer/top/500/most-followers",
+            "https://socialblade.com/mixer/top/500/most-viewed",
+            "https://socialblade.com/mixer/top/500/highest-level",
+            "https://socialblade.com/mixer/top/500/highest-sparks",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        hrefs = response.css('a::attr(href)').getall()
-        hrefs = [h for h in hrefs if '/youtube/c/' in h or '/youtube/channel/' in h or '/youtube/user/' in h]
-        page = response.url.split("/")[-2]
-        filename = 'top5000-{}.txt'.format(page)
-        with open(filename, 'w') as f:
-            f.write("\n".join(hrefs).replace("\n\n","\n"))
+        hrefs = response.css("a::attr(href)").getall()
+        platform = response.url.split("/")[3]
+        hrefs = [
+            h
+            for h in hrefs
+            if "/{}/c/".format(platform) in h
+            or "/{}/channel/".format(platform) in h
+            or "/{}/user/".format(platform) in h
+            or "/{}/page/".format(platform) in h
+        ]
+        filename = "output/{}.txt".format("_".join(filter(None, response.url.split("/")[3:])))
+        with open(filename, "w") as f:
+            f.write("\n".join(hrefs).replace("\n\n", "\n"))
 
-        self.log('Saved file {}'.format(filename))
+        self.log("Saved file {}".format(filename))
